d3de7492bda50e1d5ccc079043ed09a96ddeb18b
thien-nam dinh <thien-nam@cslinux23.cs.rice.edu>
2017-11-17 01:47:00 -0600

    mostly a bunch of renaming to conform to Tor coding standards

--- scripts/test_payments.sh
+++ scripts/test_payments.sh
@@ -2,4 +2,4 @@
 
 cd ../src
 
-gcc test/test_mt_main.c test/test_mt_crypto.c test/test_mt_tokens.c test/test_mt_utils.c test/test_mt_ledger.c mt_tokens.c mt_utils.c mt_crypto.c mt_ledger.c -o test/test_mt_main `pkg-config --cflags --libs glib-2.0` -lssl -lcrypto -Wall && test/test_mt_main
+gcc test/test_mt_main.c test/test_mt_crypto.c test/test_mt_tokens.c test/test_mt_common.c test/test_mt_ledger.c mt_tokens.c mt_common.c mt_crypto.c mt_ledger.c -o test/test_mt_main `pkg-config --cflags --libs glib-2.0` -lssl -lcrypto -Wall && test/test_mt_main
