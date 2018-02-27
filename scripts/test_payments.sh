321951796988c50ad1bc90e641e0021edc41e24f
Thien-Nam Dinh <thiennam.tnd@gmail.com>
2018-02-26 18:05:23 -0600

    cleaned out outdated files

--- scripts/test_payments.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cd ../src
-
-gcc test/test_mt_main.c test/test_mt_crypto.c test/test_mt_tokens.c test/test_mt_common.c test/test_mt_ledger.c mt_tokens.c mt_common.c mt_crypto.c mt_ledger.c -o test/test_mt_main `pkg-config --cflags --libs glib-2.0` -lssl -lcrypto -Wall && test/test_mt_main
--- src/test/test_mt_main.h
+++
@@ -1,9 +0,0 @@
-#ifndef test_main_h
-#define test_main_h
-
-void test_mt_crypto();
-void test_mt_tokens();
-void test_mt_common();
-void test_mt_ledger();
-
-#endif
